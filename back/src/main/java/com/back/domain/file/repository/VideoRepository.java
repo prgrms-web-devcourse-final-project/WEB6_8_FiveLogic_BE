003d85c647c2f8adcdf610aa995edef15bf16c65
jiseopshin <wltjq1203@icloud.com>
2025-09-19 16:04:26 +0900

    Feat : JpaRepository 상속받도록함

--- back/src/main/java/com/back/domain/file/repository/VideoRepository.java
+++ back/src/main/java/com/back/domain/file/repository/VideoRepository.java
@@ -1,7 +1,9 @@
 package com.back.domain.file.repository;
 
+import com.back.domain.file.entity.Video;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VideoRepository {
+public interface VideoRepository extends JpaRepository<Video, Integer> {
 }
