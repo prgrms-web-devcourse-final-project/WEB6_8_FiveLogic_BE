01a559103c1bd06fde9253e35431ff77c6ba424d
jiseopshin <wltjq1203@icloud.com>
2025-09-19 11:32:25 +0900

    Feat : Video 작성 및 News 와 연관관계 매핑

--- back/src/main/java/com/back/domain/news/news/entity/News.java
+++ back/src/main/java/com/back/domain/news/news/entity/News.java
@@ -1,10 +1,12 @@
 package com.back.domain.news.news.entity;
 
+import com.back.domain.file.entity.Video;
 import com.back.domain.news.comment.entity.Comment;
 import com.back.global.jpa.BaseEntity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,24 +19,25 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class News extends BaseEntity {
     private String title;
-    //todo: video entity 생성 후 통합 필요
-//    @OneToOne
-//    private Video video;
+    @OneToOne
+    private Video video;
     private String content;
     @OneToMany(mappedBy = "news", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comment;
     private Integer likes = 0;
 
     @Builder(access = AccessLevel.PRIVATE)
-    private News(String title, String content, Integer likes) {
+    private News(String title, Video video, String content, Integer likes) {
         this.title = title;
+        this.video = video;
         this.content = content;
         this.likes = likes;
     }
 
-    public static News create(String title, String content) {
+    public static News create(String title, Video video, String content) {
         return News.builder()
                 .title(title)
+                .video(video)
                 .content(content)
                 .likes(0)
                 .build();
