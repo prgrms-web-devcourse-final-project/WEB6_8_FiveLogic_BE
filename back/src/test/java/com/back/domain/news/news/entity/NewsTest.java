5591ed1f399f9f7a032ceec3e154e07e1c0e9274
jiseopshin <wltjq1203@icloud.com>
2025-09-19 12:24:03 +0900

    Test : Member 연관관계 매핑에 따른 테스트케이스 추가 및 수정

--- back/src/test/java/com/back/domain/news/news/entity/NewsTest.java
+++ back/src/test/java/com/back/domain/news/news/entity/NewsTest.java
@@ -1,7 +1,9 @@
 package com.back.domain.news.news.entity;
 
 import com.back.domain.file.entity.Video;
+import com.back.domain.fixture.MemberFixture;
 import com.back.domain.fixture.VideoFixture;
+import com.back.domain.member.member.entity.Member;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,10 +16,11 @@ class NewsTest {
         String title = "Sample News Title";
         String content = "This is a sample news content.";
         Video video = VideoFixture.createDefault();
-
-        News news = News.create(title, video, content);
+        Member member = MemberFixture.createDefault();
+        News news = News.create(member, title, video, content);
 
         assertThat(news).isNotNull();
+        assertThat(news.getMember()).isEqualTo(member);
         assertThat(news.getTitle()).isEqualTo(title);
         assertThat(news.getVideo()).isEqualTo(video);
         assertThat(news.getContent()).isEqualTo(content);
@@ -26,19 +29,34 @@ class NewsTest {
     }
 
     @Test
-    @DisplayName("제목이 null 혹은 공백인 경우 예외를 반환한다.")
-    void newsCreationTestWithInvalidTitle() {
+    @DisplayName("멤버가 null인 경우 예외를 반환한다.")
+    void newsCreationTestWithInvalidMember() {
+        String title = "Sample News Title";
         String content = "This is a sample news content.";
         Video video = VideoFixture.createDefault();
 
         try {
-            News.create(null, video, content);
+            News.create(null, title, video, content);
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Test
+    @DisplayName("제목이 null 혹은 공백인 경우 예외를 반환한다.")
+    void newsCreationTestWithInvalidTitle() {
+        Member member = MemberFixture.createDefault();
+        String content = "This is a sample news content.";
+        Video video = VideoFixture.createDefault();
+
+        try {
+            News.create(member, null, video, content);
         } catch (Exception e) {
             assertThat(e).isInstanceOf(IllegalArgumentException.class);
         }
 
         try {
-            News.create("", video, content);
+            News.create(member, "", video, content);
         } catch (Exception e) {
             assertThat(e).isInstanceOf(IllegalArgumentException.class);
         }
@@ -47,11 +65,12 @@ class NewsTest {
     @Test
     @DisplayName("영상이 null인 경우 예외를 반환한다.")
     void newsCreationTestWithInvalidVideo() {
+        Member member = MemberFixture.createDefault();
         String title = "Sample News Title";
         String content = "This is a sample news content.";
 
         try {
-            News.create(title, null, content);
+            News.create(member, title, null, content);
         } catch (Exception e) {
             assertThat(e).isInstanceOf(IllegalArgumentException.class);
         }
@@ -60,20 +79,20 @@ class NewsTest {
     @Test
     @DisplayName("내용이 null 혹은 공백인 경우 예외를 반환한다.")
     void newsCreationTestWithInvalidContent() {
+        Member member = MemberFixture.createDefault();
         String title = "Sample News Title";
         Video video = VideoFixture.createDefault();
 
         try {
-            News.create(title, video, null);
+            News.create(member, title, video, null);
         } catch (Exception e) {
             assertThat(e).isInstanceOf(IllegalArgumentException.class);
         }
 
         try {
-            News.create(title, video, "");
+            News.create(member, title, video, "");
         } catch (Exception e) {
             assertThat(e).isInstanceOf(IllegalArgumentException.class);
         }
     }
-
 }
