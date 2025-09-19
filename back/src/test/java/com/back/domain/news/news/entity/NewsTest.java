74fbd3dade37ae8fcda211a6d21fdc28700c5717
jiseopshin <wltjq1203@icloud.com>
2025-09-19 12:02:26 +0900

    Test : 테스트 추가

--- back/src/test/java/com/back/domain/news/news/entity/NewsTest.java
+++ back/src/test/java/com/back/domain/news/news/entity/NewsTest.java
@@ -21,6 +21,58 @@ class NewsTest {
         assertThat(news.getTitle()).isEqualTo(title);
         assertThat(news.getVideo()).isEqualTo(video);
         assertThat(news.getContent()).isEqualTo(content);
+        assertThat(news.getLikes()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("제목이 null 혹은 공백인 경우 예외를 반환한다.")
+    void newsCreationTestWithInvalidTitle() {
+        String content = "This is a sample news content.";
+        Video video = VideoFixture.createDefault();
+
+        try {
+            News.create(null, video, content);
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        try {
+            News.create("", video, content);
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Test
+    @DisplayName("영상이 null인 경우 예외를 반환한다.")
+    void newsCreationTestWithInvalidVideo() {
+        String title = "Sample News Title";
+        String content = "This is a sample news content.";
+
+        try {
+            News.create(title, null, content);
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Test
+    @DisplayName("내용이 null 혹은 공백인 경우 예외를 반환한다.")
+    void newsCreationTestWithInvalidContent() {
+        String title = "Sample News Title";
+        Video video = VideoFixture.createDefault();
+
+        try {
+            News.create(title, video, null);
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        try {
+            News.create(title, video, "");
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(IllegalArgumentException.class);
+        }
     }
 
 }
