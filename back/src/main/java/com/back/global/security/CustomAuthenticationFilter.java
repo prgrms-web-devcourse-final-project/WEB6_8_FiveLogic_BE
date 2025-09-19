c18696d1b1507d54fa28a867e46af00a252e5663
dbfgml20 <dbfgml20@gmail.com>
2025-09-19 11:03:08 +0900

    feat: SpringDoc 설정

--- back/src/main/java/com/back/global/security/CustomAuthenticationFilter.java
+++ back/src/main/java/com/back/global/security/CustomAuthenticationFilter.java
@@ -48,7 +48,10 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
     private void work(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // 인증이 필요없는 API 요청이라면 패스
         if (List.of("/auth/login", "/auth/signup", "/h2-console").contains(request.getRequestURI()) ||
-                request.getRequestURI().startsWith("/h2-console/")) {
+                request.getRequestURI().startsWith("/h2-console/") ||
+                request.getRequestURI().startsWith("/swagger-ui/") ||
+                request.getRequestURI().startsWith("/v3/api-docs/") ||
+                request.getRequestURI().startsWith("/swagger-resources/")) {
             filterChain.doFilter(request, response);
             return;
         }
