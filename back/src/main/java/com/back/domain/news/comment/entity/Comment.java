ace6d2dece9ec7c4eb1aee5862aa407d958fc03a
jiseopshin <wltjq1203@icloud.com>
2025-09-19 15:17:15 +0900

    Fix : News에서 추가되도록 수정

--- back/src/main/java/com/back/domain/news/comment/entity/Comment.java
+++ back/src/main/java/com/back/domain/news/comment/entity/Comment.java
@@ -44,7 +44,6 @@ public class Comment extends BaseEntity {
                 .news(news)
                 .content(content)
                 .build();
-        news.addComment(comment);
 
         return comment;
     }
