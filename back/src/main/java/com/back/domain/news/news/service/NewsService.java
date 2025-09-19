3aa9157e41430c09f3befdb09f0947a7aed0fead
jiseopshin <wltjq1203@icloud.com>
2025-09-19 15:37:29 +0900

    Feat : NewsService 작성

--- back/src/main/java/com/back/domain/news/news/service/NewsService.java
+++ back/src/main/java/com/back/domain/news/news/service/NewsService.java
@@ -1,12 +1,42 @@
 package com.back.domain.news.news.service;
 
 
+import com.back.domain.file.entity.Video;
+import com.back.domain.member.member.entity.Member;
+import com.back.domain.news.news.entity.News;
 import com.back.domain.news.news.repository.NewsRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class NewsService {
     private final NewsRepository newsRepository;
+
+    public News createNews(Member member, String title, Video video, String content) {
+        News news = News.create(member, title, video, content);
+        return newsRepository.save(news);
+    }
+
+    public Page<News> getNewsByPage(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createDate").descending());
+        return newsRepository.findAll(pageable);
+    }
+
+    public News getNewsById(Integer id) {
+        return newsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("뉴스를 찾을 수 없습니다."));
+    }
+
+    public void updateNews(News news, String title, Video video, String content) {
+        news.update(title, video, content);
+        newsRepository.save(news);
+    }
+
+    public void deleteNews(News news) {
+        newsRepository.delete(news);
+    }
 }
