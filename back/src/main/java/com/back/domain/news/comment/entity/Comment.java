6f7fdb6893400319f48a0d3905af9dcffe3a18dd
jiseopshin <wltjq1203@icloud.com>
2025-09-19 11:13:29 +0900

    Feat : Comment, News 엔티티 작성.

---
+++ back/src/main/java/com/back/domain/news/comment/entity/Comment.java
@@ -0,0 +1,39 @@
+package com.back.domain.news.comment.entity;
+
+import com.back.domain.member.member.entity.Member;
+import com.back.domain.news.news.entity.News;
+import com.back.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseEntity {
+    @ManyToOne
+    private Member member;
+    @ManyToOne
+    @JoinColumn
+    private News news;
+    private String content;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Comment(Member member, News news, String content) {
+        this.member = member;
+        this.news = news;
+        this.content = content;
+    }
+
+    public static Comment create(Member member, News news, String content) {
+        return Comment.builder()
+                .member(member)
+                .news(news)
+                .content(content)
+                .build();
+    }
+}
---
+++ back/src/main/java/com/back/domain/news/news/entity/News.java
@@ -0,0 +1,40 @@
+package com.back.domain.news.news.entity;
+
+import com.back.domain.news.comment.entity.Comment;
+import com.back.global.jpa.BaseEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class News extends BaseEntity {
+    private String title;
+    //todo: video entity 생성 후 통합 필요
+//    @OneToOne
+//    private Video video;
+    private String content;
+    @OneToMany(mappedBy = "news", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comment;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private News(String title, String content, List<Comment> comment) {
+        this.title = title;
+        this.content = content;
+        this.comment = comment;
+    }
+
+    public static News create(String title, String content) {
+        return News.builder()
+                .title(title)
+                .content(content)
+                .build();
+    }
+}
