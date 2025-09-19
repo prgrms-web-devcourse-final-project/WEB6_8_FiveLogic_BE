f330c672360b76496472c1b94d5b9527b7540c41
jiseopshin <wltjq1203@icloud.com>
2025-09-19 15:26:14 +0900

    Feat : comment 도메인 Service, Repository 작성

---
+++ back/src/main/java/com/back/domain/news/comment/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package com.back.domain.news.comment.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository {
+}
---
+++ back/src/main/java/com/back/domain/news/comment/service/CommentService.java
@@ -0,0 +1,10 @@
+package com.back.domain.news.comment.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+}
