62d1b3925538f727f7841f4659345c9a1cd37cd7
jiseopshin <wltjq1203@icloud.com>
2025-09-21 22:22:32 +0900

    Feat : 메서드 오버로딩

--- back/src/test/java/com/back/fixture/VideoFixture.java
+++ back/src/test/java/com/back/fixture/VideoFixture.java
@@ -6,7 +6,25 @@ import java.util.UUID;
 
 public class VideoFixture {
     private String uuid = UUID.randomUUID().toString();
-    private String transcodingResults = "{\"resolutions\": [\"360p\", \"720p\"]}";
+    private String transcodingResults = """
+                {
+                  "1080p": {
+                    "file_path": "/path/to/1080p/video.mpd",
+                    "status": "COMPLETED",
+                    "size_mb": 150
+                  },
+                  "720p": {
+                    "file_path": "/path/to/720p/video.mpd",
+                    "status": "IN_PROGRESS",
+                    "size_mb": 80
+                  },
+                  "480p": {
+                    "file_path": "/path/to/480p/video.mpd",
+                    "status": "FAILED",
+                    "size_mb": 40
+                  }
+                }
+                """;
     private String originalPath = "/videos/original.mp4";
     private String originalFileName = "original.mp4";
     private Integer duration = 120;
@@ -18,6 +36,16 @@ public class VideoFixture {
 
     public static Video createDefault() {
         return builder().build();
+    }
+    public static Video create(String uuid, String transcodingResults, String originalPath, String originalFileName, Integer duration, Long fileSize) {
+        return builder()
+                .withUuid(uuid)
+                .withTranscodingResults(transcodingResults)
+                .withOriginalPath(originalPath)
+                .withOriginalFileName(originalFileName)
+                .withDuration(duration)
+                .withFileSize(fileSize)
+                .build();
     }
 
     public VideoFixture withUuid(String uuid) {
