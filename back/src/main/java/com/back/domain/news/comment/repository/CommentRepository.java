1f091d554fea8e90f987c23fe07fc357483f4ca9
jiseopshin <wltjq1203@icloud.com>
2025-09-22 14:41:50 +0900

    Feat : Comment 서비스/레포지토리 작성

---
+++ back/src/main/java/com/back/domain/news/comment/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.back.domain.news.comment.repository;
+
+import com.back.domain.news.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
