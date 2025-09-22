c4bd2e970e2cc55b9e590f737a670ede5432e530
sso0om <ssooom142@gmail.com>
2025-09-22 10:52:29 +0900

    rename: 멘토링레파지토리로 명칭 변경

--- back/src/main/java/com/back/domain/mentoring/mentoring/repository/MentoringRepository.java
+++ back/src/main/java/com/back/domain/mentoring/mentoring/repository/MentoringRepository.java
@@ -3,5 +3,5 @@ package com.back.domain.mentoring.mentoring.repository;
 import com.back.domain.mentoring.mentoring.entity.Mentoring;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MentoRepository extends JpaRepository<Mentoring, Long> {
+public interface MentoringRepository extends JpaRepository<Mentoring, Long> {
 }
