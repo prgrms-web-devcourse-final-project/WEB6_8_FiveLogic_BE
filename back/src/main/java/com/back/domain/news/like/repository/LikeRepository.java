92a0e6554ebee2dcf2b84d75a409538fb209e1df
jiseopshin <wltjq1203@icloud.com>
2025-09-22 11:30:55 +0900

    Feat : 좋아요기능 작성

--- back/src/main/java/com/back/domain/news/like/repository/LikeRepository.java
+++ back/src/main/java/com/back/domain/news/like/repository/LikeRepository.java
@@ -1,9 +1,14 @@
 package com.back.domain.news.like.repository;
 
+import com.back.domain.member.member.entity.Member;
 import com.back.domain.news.like.entity.Like;
+import com.back.domain.news.news.entity.News;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Integer> {
+    Optional<Like> findByMemberAndNews(Member member, News news);
 }
