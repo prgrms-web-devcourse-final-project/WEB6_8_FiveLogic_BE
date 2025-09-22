6e0ca1e09b96ac7b453587e4bd65aba2a519682c
jiseopshin <wltjq1203@icloud.com>
2025-09-22 11:23:55 +0900

    Feat : Like 서비스, 레포지토리 작성

--- back/src/main/java/com/back/domain/news/like/repository/LikeRepository.java
+++ back/src/main/java/com/back/domain/news/like/repository/LikeRepository.java
@@ -1,7 +1,9 @@
 package com.back.domain.news.like.repository;
 
+import com.back.domain.news.like.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LikeRepository {
+public interface LikeRepository extends JpaRepository<Like, Integer> {
 }
--- back/src/main/java/com/back/domain/news/like/service/LikeService.java
+++ back/src/main/java/com/back/domain/news/like/service/LikeService.java
@@ -1,9 +1,11 @@
 package com.back.domain.news.like.service;
 
+import com.back.domain.news.like.repository.LikeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LikeService {
+    private final LikeRepository likeRepository;
 }
