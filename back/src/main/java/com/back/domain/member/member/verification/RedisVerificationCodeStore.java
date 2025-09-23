603e3ff3347417064de3591a7868f573d0c2e8db
dooongdaeng <snake10102@naver.com>
2025-09-23 15:31:28 +0900

    fix:redis관련 오류 일단 안보이게

--- back/src/main/java/com/back/domain/member/member/verification/RedisVerificationCodeStore.java
+++ back/src/main/java/com/back/domain/member/member/verification/RedisVerificationCodeStore.java
@@ -1,6 +1,8 @@
 package com.back.domain.member.member.verification;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
@@ -10,7 +12,9 @@ import java.util.Optional;
 
 @Component
 @Profile("prod")
+@ConditionalOnProperty(name = "spring.data.redis.host")
 @RequiredArgsConstructor
+@Slf4j
 public class RedisVerificationCodeStore implements VerificationCodeStore {
     private final RedisTemplate<String, String> redisTemplate;
 
