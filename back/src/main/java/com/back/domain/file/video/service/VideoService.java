bfee7493a00d0af636968eb2e2c9ff9e862d1ce9
jiseopshin <wltjq1203@icloud.com>
2025-09-28 17:08:58 +0900

    Refactor : 필드명 변경

--- back/src/main/java/com/back/domain/file/video/service/VideoService.java
+++ back/src/main/java/com/back/domain/file/video/service/VideoService.java
@@ -11,20 +11,20 @@ import org.springframework.stereotype.Service;
 public class VideoService {
     private final VideoRepository videoRepository;
 
-    public Video createVideo(String uuid, String transcodingStatus, String originalPath, Integer duration, Long fileSize) {
-        Video video = Video.create(uuid, transcodingStatus, originalPath, duration, fileSize);
+    public Video createVideo(String uuid, String status, String path, Integer duration, Long fileSize) {
+        Video video = Video.create(uuid, status, path, duration, fileSize);
         return videoRepository.save(video);
     }
 
     public Video getNewsByUuid(String uuid) {
         return videoRepository.findByUuid(uuid)
-                .orElseThrow(() -> new ServiceException("404","Video not found"));
+                .orElseThrow(() -> new ServiceException("404", "Video not found"));
     }
 
-    public Video updateStatus(String uuid, String status){
+    public Video updateStatus(String uuid, String status) {
         Video news = getNewsByUuid(uuid);
-        if (status == null|| status.isBlank()) {
-            throw new ServiceException("400","status cannot be null or empty");
+        if (status == null || status.isBlank()) {
+            throw new ServiceException("400", "status cannot be null or empty");
         }
         news.updateStatus(status);
         return videoRepository.save(news);
