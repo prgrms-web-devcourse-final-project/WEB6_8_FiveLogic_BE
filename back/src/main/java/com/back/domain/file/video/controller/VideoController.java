eeac83f612635c862a8a53d5c0c134ff3bddaa4b
jiseopshin <wltjq1203@icloud.com>
2025-09-29 09:36:52 +0900

    Feat : Operation 추가

--- back/src/main/java/com/back/domain/file/video/controller/VideoController.java
+++ back/src/main/java/com/back/domain/file/video/controller/VideoController.java
@@ -4,6 +4,7 @@ import com.back.domain.file.video.dto.controller.UploadUrlGetResponse;
 import com.back.domain.file.video.dto.service.PresignedUrlResponse;
 import com.back.domain.file.video.service.FileManager;
 import com.back.global.rsData.RsData;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ public class VideoController {
     private final FileManager fileManager;
 
     @GetMapping("/videos/upload")
+    @Operation(summary="업로드용 URL 요청", description="파일 업로드를 위한 Presigned URL을 발급받습니다.")
     public RsData<UploadUrlGetResponse> getUploadUrl() {
         PresignedUrlResponse uploadUrl = fileManager.getUploadUrl();
         UploadUrlGetResponse response = new UploadUrlGetResponse(uploadUrl.url(), uploadUrl.expiresAt());
@@ -22,6 +24,7 @@ public class VideoController {
     }
 
     @GetMapping("/videos/download")
+    @Operation(summary="다운로드용 URL 요청", description="파일 다운로드를 위한 Presigned URL을 발급받습니다.")
     public RsData<UploadUrlGetResponse> getDownloadUrls(@RequestParam String objectKey) {
         PresignedUrlResponse downloadUrl = fileManager.getDownloadUrl(objectKey);
         UploadUrlGetResponse response = new UploadUrlGetResponse(downloadUrl.url(), downloadUrl.expiresAt());
