90104e5f6733374ce746609cd23ccafefe1f147f
jiseopshin <wltjq1203@icloud.com>
2025-10-02 21:22:17 +0900

    Fix : 키값 수정

--- back/src/main/java/com/back/domain/mentoring/session/repository/MentoringSessionRepository.java
+++ back/src/main/java/com/back/domain/mentoring/session/repository/MentoringSessionRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MentoringSessionRepository extends JpaRepository<MentoringSession, Integer> {
+public interface MentoringSessionRepository extends JpaRepository<MentoringSession, Long> {
 }
