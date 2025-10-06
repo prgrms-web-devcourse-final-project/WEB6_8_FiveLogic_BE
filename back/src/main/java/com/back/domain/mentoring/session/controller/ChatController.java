0d6098362370d0f32f9799242ebff8fd40f1904f
jiseopshin <wltjq1203@icloud.com>
2025-10-07 00:43:26 +0900

    Feat : 채팅 기능 수정

--- back/src/main/java/com/back/domain/mentoring/session/controller/ChatController.java
+++ back/src/main/java/com/back/domain/mentoring/session/controller/ChatController.java
@@ -1,23 +1,26 @@
 package com.back.domain.mentoring.session.controller;
 
-import com.back.domain.member.member.entity.Member;
 import com.back.domain.mentoring.session.dto.ChatMessageRequest;
 import com.back.domain.mentoring.session.service.ChatManager;
+import com.back.global.rq.Rq;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class ChatController {
     private final ChatManager chatManager;
+    private final Rq rq;
 
     @MessageMapping({"/chat/message/{mentoringSessionUuid}"})
-    public void message(@DestinationVariable String mentoringSessionUuid, @AuthenticationPrincipal Member member, ChatMessageRequest chatMessageRequest) {
-        chatManager.chat(mentoringSessionUuid, member, chatMessageRequest);
+    public void message(@DestinationVariable String mentoringSessionUuid, Principal principal, ChatMessageRequest chatMessageRequest) {
+
+        chatManager.chat(mentoringSessionUuid, principal, chatMessageRequest);
     }
 }
