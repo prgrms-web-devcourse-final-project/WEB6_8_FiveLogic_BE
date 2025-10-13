4358948bb83e3cfbd5ce2e5df382361152f24d38
jiseopshin <wltjq1203@icloud.com>
2025-10-13 22:48:34 +0900

    Feat : url 복구

--- back/src/main/java/com/back/domain/file/video/dto/controller/UploadUrlGetResponse.java
+++ back/src/main/java/com/back/domain/file/video/dto/controller/UploadUrlGetResponse.java
@@ -3,6 +3,7 @@ package com.back.domain.file.video.dto.controller;
 import java.time.LocalDateTime;
 
 public record UploadUrlGetResponse(
+        String url,
         String uuid,
         LocalDateTime expiresAt
 ) {
