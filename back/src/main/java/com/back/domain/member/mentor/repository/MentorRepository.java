9372e29c5153e4128467f7d3f7aca7168e93a2fd
sso0om <ssooom142@gmail.com>
2025-10-13 18:40:18 +0900

    Refactor: 멘토링 조회 시 멘토 직업 함꼐 조회

--- back/src/main/java/com/back/domain/member/mentor/repository/MentorRepository.java
+++ back/src/main/java/com/back/domain/member/mentor/repository/MentorRepository.java
@@ -12,7 +12,7 @@ public interface MentorRepository extends JpaRepository<Mentor, Long> {
     @Query("SELECT m FROM Mentor m WHERE m.member.id = :memberId AND m.isDeleted = false")
     Optional<Mentor> findByMemberId(@Param("memberId") Long memberId);
 
-    @Query("SELECT m FROM Mentor m JOIN FETCH m.member WHERE m.member.id = :memberId AND m.isDeleted = false")
+    @Query("SELECT m FROM Mentor m JOIN FETCH m.member LEFT JOIN FETCH m.job WHERE m.member.id = :memberId AND m.isDeleted = false")
     Optional<Mentor> findByMemberIdWithMember(@Param("memberId") Long memberId);
 
     @Query("SELECT m FROM Mentor m WHERE m.id = :id AND m.isDeleted = false")
