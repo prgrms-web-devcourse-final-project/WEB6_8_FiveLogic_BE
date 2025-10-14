5d6776f64293680df02ab0b5d6cc6a63b5e2f39b
dooongdaeng <snake10102@naver.com>
2025-10-14 14:26:09 +0900

    Feat/236 (#323)
    * fix: DB 외래키 문제 발생 로직 주석 처리
    
    * fix: 기존과 동일하게 트랜잭션 처리하도록 수정
    
    * Revert "[Fix] 운영 DB 외래키 문제 제"
    
    * fix:conditionalonberedn an 제거
    
    ---------
    
    Co-authored-by: sso0om <ssooom142@gmail.com>
    Co-authored-by: 석희성 <lukhee1006@gmail.com>
    Co-authored-by: 신지섭 <41179427+tlswltjq@users.noreply.github.com>
    Co-authored-by: dbfgml20 <53211374+dbfgml2000@users.noreply.github.com>
    Co-authored-by: dbfgml20 <dbfgml20@gmail.com>

--- back/build.gradle.kts
+++ back/build.gradle.kts
@@ -39,7 +39,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation ("org.springframework.boot:spring-boot-starter-data-redis")
 	implementation("org.springframework.boot:spring-boot-starter-mail")
 
 	// QueryDSL
