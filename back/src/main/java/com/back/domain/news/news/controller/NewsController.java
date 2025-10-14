ed8472e42a5f64c0f7a22ce5531fb350f695a5c6
jiseopshin <wltjq1203@icloud.com>
2025-10-14 18:55:57 +0900

    Fix : 인가 수정

--- back/src/main/java/com/back/domain/news/news/controller/NewsController.java
+++ back/src/main/java/com/back/domain/news/news/controller/NewsController.java
@@ -48,9 +48,6 @@ public class NewsController {
         if (member == null) {
             return new RsData<>("401", "로그인 후 이용해주세요.", null);
         }
-        if (member.getRole() != Member.Role.ADMIN) {
-            return new RsData<>("403", "권한이 없습니다.", null);
-        }
         boolean hasUserLikedNews = newsLikeService.hasUserLikedNews(member, newsId);
 
         News news = newsService.getNewsById(newsId);
